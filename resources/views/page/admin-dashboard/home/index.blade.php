d0ab00f5bc1f0c9e6604ce09a0bc4464557d1242
Dipta Harimbawa <vipalkuda@gmail.com>
2024-01-25 13:11:20 +0700

    [fix] typing

--- resources/views/page/admin-dashboard/home/index.blade.php
+++ resources/views/page/admin-dashboard/home/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('page-link', '/')
 @section('page-title', 'Dashboard')
-@section('sub-page-title', 'Dashboard')
+@section('sub-page-title', 'Index')
 
 @section('content')
 <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5">
